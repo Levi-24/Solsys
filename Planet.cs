4eab850fb44f72cb4320ba782a2b8c521ea1929f
Ny19MajorL <Ny19MajorL@203-14>
2023-10-24 08:26:44 +0200

    Add project files.

---
+++ Planet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solsys
+{
+    internal class Planet
+    {
+        public string Nev { get; set; }
+        public int HoldSzam { get; set; }
+        public double TerfogatArany { get; set; }
+
+        public Planet(string sor)
+        {
+            var darabok = sor.Split(';');
+            this.Nev = darabok[0];
+            this.HoldSzam = int.Parse(darabok[1]);
+            this.TerfogatArany = Convert.ToDouble(darabok[2]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Solsys;
+
+List<Planet> planets = new List<Planet>();
+using (var sr = new StreamReader(@"..\..\..\src\solsys.txt"))
+{
+    sr.ReadLine();
+    while (!sr.EndOfStream)
+    {
+        planets.Add(new Planet(sr.ReadLine()));
+    }
+}
+
+Console.WriteLine(planets.Count);
